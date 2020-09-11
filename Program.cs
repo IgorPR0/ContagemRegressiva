4687da7b30b952689e406b89d793dca6b0f700eb
igor matos ribeiro <igortecnologia2020@gmail.com>
2020-09-11 12:37:54 -0300

    Feito atividade ContagemRegressiva pc

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ContagemRegressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Preparar...");
+            Console.WriteLine("Aperta qualquer tecla para continuar");
+            Console.ReadKey();
+            Console.Write("\n");
+
+            Console.WriteLine("Apontar...");
+            Console.WriteLine("Aperta qualquer tecla para continuar");
+            Console.ReadKey();
+            Console.Write("\n");
+            
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("FOGO!");
+            Console.WriteLine("Aperta qualquer tecla para continuar");
+            Console.ReadKey();
+            Console.ResetColor();
+
+        }
+    }
+}
